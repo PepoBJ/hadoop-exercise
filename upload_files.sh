f8ed1150355aced1c601cc355ec9960ed9f86bf1
Robert HC <bj112143@gmail.com>
2021-01-23 20:16:50 -0500

    Update path files

--- upload_files.sh
+++ upload_files.sh
@@ -2,13 +2,13 @@
 
 echo "Upload countries..."
 hdfs dfs -mkdir -p /user/training/upload/data/country/
-hdfs dfs -put -f /media/sf_shared/data/paises.ada /user/training/upload/data/country/
+hdfs dfs -put -f /home/maria_dev/hadoop-exercise/data/paises.ada /user/training/upload/data/country/
 echo "Upload flights..."
 hdfs dfs -mkdir -p /user/training/upload/data/flight/
-hdfs dfs -put -f /media/sf_shared/data/vuelos.dat /user/training/upload/data/flight/
+hdfs dfs -put -f /home/maria_dev/hadoop-exercise/data/vuelos.dat /user/training/upload/data/flight/
 echo "Upload delays..."
 hdfs dfs -mkdir -p /user/training/upload/data/delay/
-hdfs dfs -put -f /media/sf_shared/data/retrasos.dat /user/training/upload/data/delay/
+hdfs dfs -put -f /home/maria_dev/hadoop-exercise/data/retrasos.dat /user/training/upload/data/delay/
 echo "Upload dates..."
 hdfs dfs -mkdir -p /user/training/upload/data/date/
-hdfs dfs -put -f /media/sf_shared/data/fecha.dat /user/training/upload/data/date/
+hdfs dfs -put -f /home/maria_dev/hadoop-exercise/data/fecha.dat /user/training/upload/data/date/
