c1fbae50b7b1a75e9e735c2c65fbb0dd45178278
Robert HC <bj112143@gmail.com>
2021-01-24 14:53:35 -0500

    Fix: fixing wrong in pyspark

--- queries_data.py
+++ queries_data.py
@@ -13,7 +13,7 @@ arrive_df.show(5, False)
 country_df = spark.sql('SELECT country_code, country_name FROM t_country')
 
 take_arrive_country_df = country_df.join(take_off_df, on='country_code', how='left').join(arrive_df, on='country_code', how='left').orderBy('country_name')
-take_arrive_country_df.show(50, Fasle)
+take_arrive_country_df.show(50, False)
 
 number_flight_df = spark.sql('SELECT date_log, day_number, count(*) as number_flight FROM t_flight_history GROUP BY day_number, date_log ORDER BY number_flight DESC')
 number_flight_df.show(5, False)
@@ -22,7 +22,7 @@ flight_time_df = spark.sql('SELECT date_log, day_number, avg(delay_time) as dela
 flight_time_df.show(5, False)
 
 number_flight_time_df = number_flight_df.join(flight_time_df, on=['date_log', 'day_number'], how='full')
-number_flight_time_df.show(50, Fasle)
+number_flight_time_df.show(50, False)
 
 take_arrive_country_df.write.mode("overwrite").saveAsTable("indra_exercise.t_take_off_arrive_flight")
 number_flight_time_df.write.mode("overwrite").saveAsTable("indra_exercise.t_number_time_flight")
