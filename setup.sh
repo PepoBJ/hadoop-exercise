ce3a78475ae26628c48e9686d861663b526aa90d
Robert HC <bj112143@gmail.com>
2021-01-25 17:55:38 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-printf "${GREEN}> UPLOAD FILES TO HDFS${NC}"
+printf "${GREEN}> UPLOAD FILES TO HDFS${NC}\n"
 ./upload_files.sh
 
-printf "${GREEN}> CREATE TABLES HIVE${NC}"
+printf "${GREEN}> CREATE TABLES HIVE${NC}\n"
 hive -f create_tables.hql;
 echo "Done."
 
-printf "${GREEN}> EXECUTE QUERIES IN SPARK${NC}"
+printf "${GREEN}> EXECUTE QUERIES IN SPARK${NC}\n"
 export PYTHONIOENCODING=utf8
 spark-submit queries_data.py
