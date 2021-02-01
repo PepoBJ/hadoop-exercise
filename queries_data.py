d6df1170f8c77c9bfeea718f26ea5fd27f52bb40
Robert HC <bj112143@gmail.com>
2021-01-31 21:32:08 -0500

    Update: change database name

--- queries_data.py
+++ queries_data.py
@@ -3,7 +3,7 @@ from pyspark.sql.functions import *
 
 print('Initializing job...')
 spark = SparkSession.builder.enableHiveSupport().getOrCreate()
-spark.sql('use indra_exercise')
+spark.sql('use air_tribu')
 
 print('> The countries with the most take-offs:')
 take_off_df = spark.sql('SELECT country_origin_code as country_code, count(*) as take_off FROM t_flight_history GROUP BY country_origin_code ORDER BY take_off DESC')
@@ -35,12 +35,12 @@ print('> [+] Consolidated table with countries with delayed flights:')
 country_delay_flight_df = spark.sql('SELECT country_origin_code, country_origin_name, avg(delay_time) as delay_time FROM t_flight_history GROUP BY country_origin_code, country_origin_name ORDER BY delay_time DESC')
 country_delay_flight_df.show(50, False)
 
-print('\n\nWriting the consolidated table <indra_exercise.t_take_off_arrive_flight> in HIVE...')
-take_arrive_country_df.write.mode("overwrite").saveAsTable("indra_exercise.t_take_off_arrive_flight")
-print('Writing the consolidated table <indra_exercise.t_number_time_flight> in HIVE...')
-number_flight_time_df.write.mode("overwrite").saveAsTable("indra_exercise.t_number_time_flight")
-print('Writing the consolidated table <indra_exercise.t_delay_country_flight> in HIVE...')
-country_delay_flight_df.write.mode("overwrite").saveAsTable("indra_exercise.t_delay_country_flight")
+print('\n\nWriting the consolidated table <air_tribu.t_take_off_arrive_flight> in HIVE...')
+take_arrive_country_df.write.mode("overwrite").saveAsTable("air_tribu.t_take_off_arrive_flight")
+print('Writing the consolidated table <air_tribu.t_number_time_flight> in HIVE...')
+number_flight_time_df.write.mode("overwrite").saveAsTable("air_tribu.t_number_time_flight")
+print('Writing the consolidated table <air_tribu.t_delay_country_flight> in HIVE...')
+country_delay_flight_df.write.mode("overwrite").saveAsTable("air_tribu.t_delay_country_flight")
 print('Done.')
 
 
